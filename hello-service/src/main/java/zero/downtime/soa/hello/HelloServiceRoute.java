5e510daee9d852c6efc8fa137c1b49979c22f91c
Rob Mills <me@robgmills.com>
2015-04-04 16:20:33 -0500

    Implement basic Hello Service

---
+++ hello-service/src/main/java/zero/downtime/soa/hello/HelloServiceRoute.java
@@ -0,0 +1,24 @@
+package zero.downtime.soa.hello;
+
+import org.apache.camel.LoggingLevel;
+import org.apache.camel.builder.RouteBuilder;
+
+/**
+ * A simple OSGi micro service route that echoes a name in a
+ * welcome message.
+ *
+ * @author Rob Mills
+ * @version 1.0
+ * @since 1.0
+ */
+public class HelloServiceRoute extends RouteBuilder {
+
+	@Override
+	public void configure() throws Exception {
+		from("direct-vm:hello").id("HelloService")
+				.log(LoggingLevel.INFO, ":::: Hello ${body}! ::::")
+				.setBody( simple("Hello ${body}!") )
+		.end();
+	}
+
+}
---
+++ hello-service/src/test/java/zero/downtime/soa/hello/HelloServiceRouteTest.java
@@ -0,0 +1,42 @@
+package zero.downtime.soa.hello;
+
+import org.apache.camel.EndpointInject;
+import org.apache.camel.builder.AdviceWithRouteBuilder;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.junit.Test;
+
+/**
+ * @author Rob Mills
+ * @version 1.0
+ * @since 1.0
+ */
+public class HelloServiceRouteTest extends CamelTestSupport {
+
+	@EndpointInject(uri = "mock:result")
+	protected MockEndpoint resultEndpoint;
+
+	@Test
+	public void testHelloRoute() throws Exception {
+		context.getRouteDefinition("HelloService").adviceWith(context, new AdviceWithRouteBuilder() {
+			@Override public void configure() throws Exception {
+				weaveAddFirst().setBody(constant("Rob"));
+				weaveAddLast().to(resultEndpoint);
+			}
+		});
+
+		context.start();
+
+		resultEndpoint.expectedMessageCount(1);
+		resultEndpoint.expectedBodiesReceived("Hello Rob!");
+
+		template.sendBodyAndHeaders( "direct-vm:hello", "Rob", null );
+		assertMockEndpointsSatisfied();
+	}
+
+	@Override
+	protected RouteBuilder createRouteBuilder() {
+		return new HelloServiceRoute();
+	}
+}
