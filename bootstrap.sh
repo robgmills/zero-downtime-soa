7d19daa709e92d40b6a690e386388c76e920918b
Rob Mills <me@robgmills.com>
2015-04-04 16:21:12 -0500

    Add Karaf to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+clear_opt() {
+	rm -r opt/
+}
+
 install_kafka() {
 	mkdir opt
 	wget -O opt/kafka.tgz http://mirror.reverse.net/pub/apache/kafka/$2/kafka_$1-$2.tgz
@@ -8,4 +12,14 @@ install_kafka() {
 	rm opt/kafka.tgz
 }
 
+install_karaf() {
+	mkdir opt
+	wget -O opt/karaf.tgz http://apache.mirrors.lucidnetworks.net/karaf/$1/apache-karaf-$1.tar.gz
+	tar -C opt/ -xf opt/karaf.tgz
+	ln -s apache-karaf-$1 opt/karaf
+	rm opt/karaf.tgz
+}
+
+clear_opt
 install_kafka 2.10 0.8.2.0
+install_karaf 3.0.3
