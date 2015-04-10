7bb0058aeaa9b8c8068bef2a5dadc3ced8a1e467
Rob Mills <me@robgmills.com>
2015-04-09 20:26:04 -0500

    Move kafka endpoint uri to config

--- hello-service-api/src/main/java/zero/downtime/soa/api/HelloServiceApi.java
+++ hello-service-api/src/main/java/zero/downtime/soa/api/HelloServiceApi.java
@@ -1,7 +1,9 @@
 package zero.downtime.soa.api;
 
 import org.apache.camel.builder.RouteBuilder;
-import org.eclipse.jetty.server.Server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
 
 /**
  * Rest DSL using the jetty component to expose the Hello Service API
@@ -12,8 +14,9 @@ import org.eclipse.jetty.server.Server;
  */
 public class HelloServiceApi extends RouteBuilder {
 
-	@Override
-	public void configure() throws Exception {
+	private static String KAFKA = "kafka:{{helloApi.kafkaCluster}}?topic={{helloApi.kafkaTopic}}&serializerClass={{helloApi.kafkaSerializer}}";
+
+	@Override public void configure() throws Exception {
 		//@formatter:off
 		restConfiguration().component("jetty")
 			.host("localhost")
@@ -22,26 +25,19 @@ public class HelloServiceApi extends RouteBuilder {
 		rest("/rest-api")
 			.get("/sync/{user}").to("direct-vm:hello")
 			.get("/async/{user}").route().transform(header("user"))
-			.to("kafka:localhost:9093,localhost:9094?topic=test&serializerClass=kafka.serializer.StringEncoder")
+			.to(KAFKA)
 			.setBody(simple("Thanks ${header.user} your messages is being processed. \\n"));
 		//@formatter:on
 	}
 
 	/**
-	 * Looks up an available port number on the host
-	 * by creating a dummy Jetty Server.
-	 *
-	 * TODO: there's probably a better way to do this
+	 * Looks up an available port number on the host.
 	 *
 	 * @return a port number available for use
-	 * @throws Exception
+	 * @throws IOException if unable to find an available ServerSocket
 	 */
-	protected int getRandomServerPort() throws Exception {
-		Server server = new Server(0);
-		server.start();
-		int port = server.getConnectors()[0].getLocalPort();
-		server.stop();
-		return port;
+	protected int getRandomServerPort() throws IOException {
+		return (new ServerSocket(0)).getLocalPort();
 	}
 }
 
